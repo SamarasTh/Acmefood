4690adeb2c663538a6a72beeaee133bb9d40ab6d
Thanos Sam <a.samaras@pmmretail.com>
2022-08-26 18:28:39 +0300

    add basic Repository classes extending Jpa

---
+++ src/main/java/gr/acmefood/repository/Product.java
@@ -0,0 +1,10 @@
+package gr.acmefood.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Product extends JpaRepository<Product, Long> {
+
+
+}
---
+++ src/main/java/gr/acmefood/repository/Store.java
@@ -0,0 +1,9 @@
+package gr.acmefood.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Store extends JpaRepository<Store,Long> {
+
+}
